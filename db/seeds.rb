db10ecae2f5fc8c10d6b1c1629785fae602cf221
Michael Garriss <mgarriss@gmail.com>
2010-06-12 17:19:31 -0600

    eve sql files load with rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-
 APP_CONFIG['admins'].each do |admin|
   Admin.create(admin) rescue (puts "not adding: #{admin['email']}")
+end
+
+def load_eve_sql_file file
+  yaml = YAML.load_file(File.join(Rails.root,'config','database.yml'))
+  db = yaml[Rails.env]
+  if db['password'].blank?
+    `mysql #{db['database']} -u#{db['username']} < #{file}`
+  else
+    `mysql #{db['database']} -u#{db['username']} -p#{db['password']} < #{file}`
+  end
+end
+
+Dir[File.join(Rails.root,'db','eve',APP_CONFIG['eve_version']) + "/*.sql"].each do |file|
+  puts "loading: #{File.basename(file)}"
+  load_eve_sql_file file
 end
