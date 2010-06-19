3ced2efc8d7d68ec73297a792a6fb7f1044826e9
Michael Garriss <mgarriss@gmail.com>
2010-06-19 11:13:20 -0600

    update to meta group

--- app/controllers/meta_groups_controller.rb
+++ app/controllers/meta_groups_controller.rb
@@ -2,7 +2,7 @@ class MetaGroupsController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @meta_groups = MetaGroup.order(@sort).paginate(:page => params[:page], :per_page => MetaGroup.per_page)
+    @meta_groups = MetaGroup.order(@sort['meta_groups']).paginate(:page => params[:page], :per_page => MetaGroup.per_page)
   end
   
   def show
