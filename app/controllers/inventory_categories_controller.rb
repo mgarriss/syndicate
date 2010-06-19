ced45901746eb428377d048faa65191f2ed705a7
Michael Garriss <mgarriss@gmail.com>
2010-06-19 11:12:04 -0600

    update to inventory categories

--- app/controllers/inventory_categories_controller.rb
+++ app/controllers/inventory_categories_controller.rb
@@ -2,10 +2,13 @@ class InventoryCategoriesController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @inventory_categories = InventoryCategory.order(@sort).paginate(:page => params[:page], :per_page => InventoryCategory.per_page)
+    @inventory_categories = InventoryCategory.order(@sort['inventory_categories']).paginate(
+      :page => params[:page], :per_page => InventoryCategory.per_page)
   end
   
   def show
-    @inventory_category = InventoryCategory.find(params[:id])
+    @inventory_category = InventoryCategory.includes(:groups, :inventory_types).find(params[:id])
+    @groups = @inventory_category.groups.order(@sort['inventory_categories'])
+    @inventory_types = @inventory_category.inventory_types.order(@sort['inventory_types'])
   end
 end
--- app/models/inventory_category.rb
+++ app/models/inventory_category.rb
@@ -4,4 +4,7 @@ class InventoryCategory < ActiveRecord::Base
 
   cattr_reader :per_page
   @@per_page = 180
+  
+  has_many :groups, :foreign_key => 'categoryID'
+  has_many :inventory_types, :through => :groups
 end
