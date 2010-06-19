bcc95fb31fd07d5ae31dc3b8ef14aae55dbde23b
Michael Garriss <mgarriss@gmail.com>
2010-06-19 11:10:41 -0600

    updates to race model

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -2,7 +2,7 @@ class RacesController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @races = Race.order(@sort).paginate(:page => params[:page], :per_page => Race.per_page)
+    @races = Race.order(@sort['races']).paginate(:page => params[:page], :per_page => Race.per_page)
   end
   
   def show
